46215aed9f23d2bcbf50c637abed763e0bf3e66b
HumbertoSimon <humbertosimon08@aragon.unam.mx>
2023-10-11 15:46:31 -0600

    se actualiza una parte

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicios 05/10/2023</title>
+    <title>Calificaciones</title>
 </head>
 <body>
 
